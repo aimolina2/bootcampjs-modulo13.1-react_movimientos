992f5be692bb09974dda38eb7be747fc3c6fb2ef
Ainoa <aimmolina2@gmail.com>
2026-03-22 21:42:48 +0100

    movements section to deliver

--- src/pages/movement-list/movement-list.page.tsx
+++ src/pages/movement-list/movement-list.page.tsx
@@ -24,7 +24,6 @@ export const MovementListPage: React.FC = () => {
       );
       getAccountInfo(id).then((result) => setAccountInfo(result));
     }
-    console.log(accountInfo);
   }, []);
 
   return (
